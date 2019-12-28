6041729bbdc441e66d4437266ab3942d8cef6db6
Jędrzej Lewandowski <jedrzej@pitchup.com>
2019-12-28 14:33:44 +0000

    feat: auth helper

---
+++ src/FirebaseAuthHelper.ts
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app';
+import * as firebaseui from 'firebaseui';
+
+export class FirebaseAuthHelper {
+  public static startFirebaseAuthUI(
+    id: string,
+    signInSuccessfulUrl: string,
+    signInProviders: string[],
+  ) {
+    const uiConfig = {
+      signInSuccessUrl: signInSuccessfulUrl,
+      signInOptions: signInProviders,
+    };
+
+        FirebaseAuthHelper.UI_INSTANCE = //eslint-disable-line
+            FirebaseAuthHelper.UI_INSTANCE || new firebaseui.auth.AuthUI(firebase.auth());
+    FirebaseAuthHelper.UI_INSTANCE.start(id, uiConfig);
+  }
+
+    private static UI_INSTANCE: firebaseui.auth.AuthUI | undefined = undefined;
+}
