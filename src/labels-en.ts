065a52c7247e2779403474f81f6ab4876311a6d3
Jędrzej Lewandowski <jedrzej@pitchup.com>
2019-12-28 14:33:57 +0000

    feat: labels in english

---
+++ src/labels-en.ts
@@ -0,0 +1,17 @@
+import { Labels } from './Config';
+
+export const firestoreRolesVueAuthLabelsEn: Labels = {
+  checkingRole: 'Checking role',
+  missingRole: 'Missing role',
+  checkingIfSufficientRole: 'Checking if your role is sufficient',
+  insufficientRole: 'Your role is insufficient',
+  yourAccountIsNotConfirmed: 'Your account is not confirmed',
+  requestAnAccess: 'Request access',
+  requestAccessExplanation:
+        'After clicking on the Request button, role admin will be able to accept it. Remember that he does not get any notification on pending requests so you have to tell him to check the list.',
+  requestAccessSuccess: 'Successfully asked for the permission',
+  alreadyRequestingTheRole: 'This role was already requested',
+  checkRoleAgain: 'Check role again',
+  logInPanelTitle: 'Please log in',
+  loading: 'Loading',
+};
